b1b9caf9b173050f9a913fa40ead072de960c048
seankrummel <sean.krummel@gmail.com>
2019-01-16 15:44:11 -0600

    completed mergeing arrays

--- array.js
+++ array.js
@@ -1,92 +1,93 @@
+'use strict';
 const memory = require('./memory');
 
 const myMemory = new memory();
 
 class Array {
-    constructor() {
-        this.length = 0;
-        this._capacity = 0;
-        this.ptr = myMemory.allocate(this.length);
+  constructor() {
+    this.length = 0;
+    this._capacity = 0;
+    this.ptr = myMemory.allocate(this.length);
+  }
+
+  push(value) {
+    if (this.length >= this._capacity) {
+      this._resize((this.length + 1) * Array.SIZE_RATIO);
     }
 
-    push(value) {
-        if (this.length >= this._capacity) {
-            this._resize((this.length + 1) * Array.SIZE_RATIO);
-        }
+    myMemory.set(this.ptr + this.length, value);
+    this.length++;
+  }
 
-        myMemory.set(this.ptr + this.length, value);
-        this.length++;
+  _resize(size) {
+    const oldPtr = this.ptr;
+    this.ptr = myMemory.allocate(size);
+    if (this.ptr === null) {
+      throw new Error('Out of memory');
+    }
+    myMemory.copy(this.ptr, oldPtr, this.length);
+    myMemory.free(oldPtr);
+    this._capacity = size;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) {
+      throw new Error('Index error');
+    }
+    return myMemory.get(this.ptr + index);
+  }
+
+  pop() {
+    if (this.length == 0) {
+      throw new Error('Index error');
+    }
+    const value = myMemory.get(this.ptr + this.length - 1);
+    this.length--;
+    return value;
+  }
+
+  insert(index, value) {
+    if (index < 0 || index >= this.length) {
+      throw new Error('Index error');
     }
 
-    _resize(size) {
-        const oldPtr = this.ptr;
-        this.ptr = myMemory.allocate(size);
-        if (this.ptr === null) {
-            throw new Error('Out of memory');
-        }
-        myMemory.copy(this.ptr, oldPtr, this.length);
-        myMemory.free(oldPtr);
-        this._capacity = size;
+    if (this.length >= this._capacity) {
+      this._resize((this.length + 1) * Array.SIZE_RATIO);
     }
 
-    get(index) {
-        if (index < 0 || index >= this.length) {
-            throw new Error('Index error');
-        }
-        return myMemory.get(this.ptr + index);
+    memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
+    memory.set(this.ptr + index, value);
+    this.length++;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) {
+      throw new Error('Index error');
     }
-
-    pop() {
-        if (this.length == 0) {
-            throw new Error('Index error');
-        }
-        const value = myMemory.get(this.ptr + this.length - 1);
-        this.length--;
-        return value;
-    }
-
-    insert(index, value) {
-        if (index < 0 || index >= this.length) {
-            throw new Error('Index error');
-        }
-
-        if (this.length >= this._capacity) {
-            this._resize((this.length + 1) * Array.SIZE_RATIO);
-        }
-
-        memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
-        memory.set(this.ptr + index, value);
-        this.length++;
-    }
-
-    remove(index) {
-        if (index < 0 || index >= this.length) {
-            throw new Error('Index error');
-        }
-        memory.copy(this.ptr + index, this.ptr + index + 1, this.length - index - 1);
-        this.length--;
-    }
+    memory.copy(this.ptr + index, this.ptr + index + 1, this.length - index - 1);
+    this.length--;
+  }
 }
 
 function main(){
-    Array.SIZE_RATIO = 3;
-    //create an instance of the array class
-    let arr = new Array();
-    //add an item to the array
-    // arr.push(3);
-    // arr.push(5);
-    // arr.push(15);                // Q1 => length = 1, capacity = 3, address = 0
-    // arr.push(19);
-    // arr.push(45);
-    // arr.push(10);                // Q2 => length = 6, capacity = 12, address = 3
-    // arr.pop();
-    // arr.pop();
-    // arr.pop();                   // Q3 => length = 3, capacity = 12, address = 3 -> capacity is set when a push is made, it does not need to be reset when a pop happens.
-    // console.log(arr.get(0))
-    // arr.push("tauhida");
-    // console.log(arr.get(0))      // Q4 => NaN -> is a nan because the index is expecting a number not a string.
-                                    // Q5 => resize changes the capacity of the array.
-    //console.log(arr);
+  Array.SIZE_RATIO = 3;
+  //create an instance of the array class
+  let arr = new Array();
+  //add an item to the array
+  // arr.push(3);
+  // arr.push(5);
+  // arr.push(15);                // Q1 => length = 1, capacity = 3, address = 0
+  // arr.push(19);
+  // arr.push(45);
+  // arr.push(10);                // Q2 => length = 6, capacity = 12, address = 3
+  // arr.pop();
+  // arr.pop();
+  // arr.pop();                   // Q3 => length = 3, capacity = 12, address = 3 -> capacity is set when a push is made, it does not need to be reset when a pop happens.
+  // console.log(arr.get(0))
+  // arr.push("tauhida");
+  // console.log(arr.get(0))      // Q4 => NaN -> is a nan because the index is expecting a number not a string.
+  // Q5 => resize changes the capacity of the array.
+  //console.log(arr);
 }
 
 main();
@@ -138,24 +139,23 @@ main();
 
 // Merge Arrays
 const merge = (arr1, arr2) => {
-    mergedArr = [];
-    currentIndex1 = 0;
-    currentIndex2 = 0;
-    
-    while(currentIndex1 <= arr1.length && currentIndex2 <= arr2.length) {
-        if(arr1[currentIndex1] < arr2[currentIndex2]) {
+    const mergedArr = [];
+    let currentIndex1 = 0;
+    let currentIndex2 = 0;
+
+    while(currentIndex1 !== arr1.length || currentIndex2 !== arr2.length) {
+        if(currentIndex2 === arr2.length || arr1[currentIndex1] <= arr2[currentIndex2]) {
             mergedArr.push(arr1[currentIndex1]);
-            console.log(currentIndex1)
-            currentIndex1++
+            currentIndex1++;
+            // console.log('1', currentIndex1, arr1[currentIndex1-1]);
         }
         else {
             mergedArr.push(arr2[currentIndex2]);
-            console.log(currentIndex1)
-            currentIndex2++
+            currentIndex2++;
+            // console.log('2', currentIndex2, arr2[currentIndex2-1]);
         }
     }
 
-    return mergedArr;
-}
-console.log(3 < undefined)
-console.log(merge([1,3,6,8,11],[2,3,5,8,9,10]))
+  return mergedArr;
+};
+console.log(merge([1,3,6,8,11],[2,3,5,8,9,10]));
